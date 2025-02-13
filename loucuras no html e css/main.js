4efa4ad10cdbb576481aa847b7e9cc1d3bd33825
TimoteoMaiaSousa <timoteomaiasousa@gmail.com>
2025-02-13 14:07:28 -0300

    not

---
+++ loucuras no html e css/main.js
@@ -0,0 +1,12 @@
+function a(){
+    var x = document.getElementById("d");
+    x.style.backgroundColor = 'black';
+
+    
+}
+
+function b(){
+    var y = document.getElementById("d");
+    y.style.backgroundColor = 'white';
+}
+
